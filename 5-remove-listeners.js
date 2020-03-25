3dd046b09fa14bc5c4235114242e3a2167113402
James Barnett <james.barnett.jr@gmail.com>
2020-03-25 00:59:38 -0500

    Sorts the scripts the way I want them

---
+++ 5-remove-listeners.js
@@ -0,0 +1,8 @@
+var removeListeners = function() {
+  $.cometd.removeListener(bigWordsFilterListener);
+  $.cometd.removeListener(gihFilterListener);
+  $.cometd.removeListener(fcFilterListener);
+  $.cometd.removeListener(vicFilterListener); 
+};
+
+
