7d957addfb4ae6ddb51134d0cc42ebba84012236
James Barnett <james.barnett.jr@gmail.com>
2020-03-26 08:55:59 -0500

    Minor improvement to script combining script

--- combine_filters.rb
+++ combine_filters.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 
-files = Dir.glob '*.js'
-
+`rm fc-master-filter.js` if File.exists? 'fc-master-filter.js'
+files = Dir['*.js'].sort
 MASTER_SHIELD = 'fc-master-filter.js'
-
 content = files.map { |f| `cat #{f}` }.join "\n"
-
 File.write MASTER_SHIELD, content
+
