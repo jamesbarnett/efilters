8b621d32912875e06e7340073ead7be296e53252
James Barnett <james.barnett.jr@gmail.com>
2020-03-28 07:36:06 -0500

    Adds function to expire manual bans

--- 0-common.js
+++ 0-common.js
@@ -1,5 +1,3 @@
-var _banDuration = 10 * 60 * 1000; // Ban accout for 10 minutes
-
 // When we find someone bad, we kick them out and remove their posts
 var kickAccountAndRemovePosts = function(userUuid) {
   CometdModerator.kickAccount(userUuid);
@@ -63,38 +61,27 @@ var findUserByName = function(username) {
  */
 var timeoutUser = function(username, duration) {
   durationInMs = duration * 60 * 1000;
- 
-  var re = new RegExp(username + "\$");
-
-  // need to lookup account
-  $.ajax({
-    url : '/search/users',
-    method : 'POST',
-    data : {
-      name : username
-    },
-    success : function(jsonReply) { 
-      var accounts = JSON.parse(jsonReply);
-      
-      accounts = accounts.filter(function(e) {
-        return e.data.username.match(re);
-      });
-
-      if (accounts.length == 1) {
-        var userUuid = accounts[0].data.userUuid;
-        tempBan(userUuid, username, duration);
-      } else {
-        console.log("Search for username " + username + " returned multiple results");
-      }
-    },
-    error : function() {
-      console.log("An error occurred.");
-    }
-  });
+  var user = findUserByName(username); 
+  
+  if (user) {
+    tempBan(user.userUuid, username, duration);
+  } else {
+    console.log('timeoutUser failed');
+  }
 };
 
+/*
+ * Call this function from the console to set an exipiration on a manual ban.
+ *   username - The user's nickname
+ *   duration - Number of minutes to ban the user
+ */
 var autoClearBan = function(username, duration) {
   var user = findUserByName(username);
-  scheduleUnban(username, user.userUuid, duration);
+  
+  if (user) {
+    scheduleUnban(username, user.userUuid, duration);
+  } else {
+    console.log("autoClearBan failed");
+  }
 };
 
