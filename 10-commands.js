5064ce4475f89c6838fd881075176e14bc4ff08c
James Barnett <james.barnett.jr@gmail.com>
2020-04-23 12:35:15 -0500

    Adds a rules command for NC

--- 10-commands.js
+++ 10-commands.js
@@ -1,17 +1,17 @@
 var ncUserUuid = '5b1d27dc-c1a6-4b97-9107-1665881d654a';
 
-var rulesMessage = [ "ROOM RULES:",
-                     "1. Respect and tolerance",
-                     "2. No F-Bombs",
-                     "3. No Religion or Politics",
-                     "4. No Soliciting",
-                     "5. Follow The Golden Rule: Treat other as you would like to be treated"
-                   ];
+var rulesMessage = "ROOM RULES:\n" + 
+                   "1. Respect and tolerance\n" +
+                   "2. No F-Bombs\n" +
+                   "3. No Religion or Politics\n" + 
+                   "4. No Soliciting\n" +
+                   "5. Follow The Golden Rule: Treat other as you would like to be treated";
 
-var commandProccessor = function(x) {
+var commandProcessor = function(x) {
   if (x.data.userUuid == ncUserUuid && x.data.messageBody == '!rules') {
-    rulesMessage.forEach((rule) => CometdRoom.sendMessage(rule));
+    CometdRoom.sendMessage(rulesMessage);
   }
 };
 
-var commandsListener = $.cometd.addListener('/chatroom/message/add/*', commandProccesor); 
+var commandsListener = $.cometd.addListener('/chatroom/message/add/*', commandProcessor); 
+
