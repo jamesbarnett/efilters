9c7faebb0cb77b9cfff1ed0d404273ae73b30460
James Barnett <james.barnett.jr@gmail.com>
2020-04-29 10:26:51 -0500

    Bug fix

--- 12-tsp-stalker.js
+++ 12-tsp-stalker.js
@@ -1,9 +1,13 @@
 var tspStalkerFilter = function(x) {
-  var prunedMsg = x.data.messageBody.toUpperCase().replace(/\,/);
+  var prunedMsg = x.data.messageBody.toUpperCase().replace(/\,/).replace('#', '');
 
-  if (prunedMsg.match(/\b(MS)TSP\b/) && prunedMsg.match(/\bDAY(\s)d+\b/)) {
-    CometdModerator.removeAccountMessages(x.user.userUuid);
+  console.log("Matched 1st: %O", prunedMsg.match(/(MS)?TSP/));
+  console.log("Matched 2nd: %O", prunedMsg.match(/\bDAY(\s)?\d+\b/));
+
+  if (prunedMsg.match(/(MS)?TSP/) && prunedMsg.match(/\bDAY(\s)?\d+\b/)) {
+    CometdModerator.removeAccountMessages(x.data.userUuid);
   }
 };
 
-var tspStalkerFilterListener = $.cometd.addListener('/chatroom/message/add', tspStalkerFilter);
+var tspStalkerFilterListener = $.cometd.addListener('/chatroom/message/add/*', tspStalkerFilter);
+
