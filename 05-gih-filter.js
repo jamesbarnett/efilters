b0d55b2e857f06810431430ee1e6a2087fe2b800
Jim Barnett <james.barnett.jr@gmail.com>
2020-05-21 10:59:33 -0500

    Adds filter for GiH favorite emojis

--- 05-gih-filter.js
+++ 05-gih-filter.js
@@ -1,8 +1,17 @@
 var gihPhrases = [/\bRAPE A BABY/, /\bRAPE BABIES/, /\bRAPES\b/, /\bRAPED\b/, /\bRαPED?\b/];
+var gihChars = [56384, 56389, 56438, 56442];
+
+var checkGihChars = function(message) {
+  for (var i = 0; i < message.length; ++i) {
+    if (gihChar.includes(message.charCodeAt(i))) {
+      return true;
+    }
+  }
+};
 
 var checkGihMessages = function(message) {
   var uc = message.toUpperCase().replace(/\./g, '').replace(/,/, '');
- 
+
   var result = null;
   gihPhrases.find(function(phrase) {
     result = uc.match(phrase) || result;
@@ -14,7 +23,7 @@ var checkGihMessages = function(message) {
 var gihFilter = function(x) {
   var message = x.data.messageBody.toUpperCase();
 
-  if (checkGihMessages(message)) {
+  if (checkGihMessages(message) || checkGihChars(message)) {
     CometdModerator.kickAccount(x.data.userUuid);
     console.log("GiHFilter: kicking user: " + x.data.username);
     CometdModerator.removeAccountMessages(x.data.userUuid);
